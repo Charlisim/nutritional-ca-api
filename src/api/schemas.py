fc7f79850b67a85c0168634fe185df3151a5ba02
Carlos Simon <git@csimon.dev>
2021-11-01 12:23:39 +0000

    wip api

--- src/api/schemas.py
+++
@@ -1,7 +0,0 @@
-from core import ma
-from marshmallow import fields
-
-
-class CompanySchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
-    name = fields.String()
--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,17 +1,26 @@
 import os
 from flask import Flask
 from flask_marshmallow import Marshmallow
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-app.debug = False
-ma = Marshmallow()
+app.debug = True
+api = Api(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["FLASK_ADMIN_SWATCH"] = "flatly"
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.url_map.strict_slashes = False
 
-from views import *
-from api.resources import *
 from admin_views import *
+from api.resources import CompanyListResource
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"hello": "world"}
+
+
+api.add_resource(HelloWorld, "/")
